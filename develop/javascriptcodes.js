278f8e7be48783a203b09e451e4d911d77e8a04c
Victor <obinnavictoronah84@gmail.com>
2023-11-27 05:46:52 +0100

    i added a new file called javascriptcodes.js

---
+++ develop/javascriptcodes.js
@@ -0,0 +1,24 @@
+// arrays
+const jsssubjects = [maths,english,CCA,CRS,basicscience,literature,igbo,french, basictechnology,agriculture]
+const sssubjects = [maths,english,biology,physics,chemistry,agriculture,animalhusbandary,economics,igbo]
+
+// objects
+const Pen = {color:"blue",
+height:"16cm",
+product:"avanti"}
+
+const motorcycle = {
+name:"hunda",
+color:"red",
+year:"2019",
+}
+
+const laptop ={
+    name:"hp",
+    processor:"intel(R)core(TM)is-4300U CPU @1.90GHz 2.50GHz",
+    RAM:"8.00 GB",
+    DeviceID:"C29A2E7A-522A-480D-B512-3DA3BED8DB24",
+    Systemtype:"64-bit operating system, x64-based processor"
+}
+
+// functions
